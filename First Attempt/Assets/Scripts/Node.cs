50baa9df23b150fdf55ee64f794496cfffa54b30
Matthew Dawley <44911280+mddawley@users.noreply.github.com>
2018-11-16 14:59:26 -0500

    Add remaining pellets; add basic movement; begin Node scripting

--- First Attempt/Assets/Scripts/Node.cs
+++ First Attempt/Assets/Scripts/Node.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class node : MonoBehaviour {
+public class Node : MonoBehaviour {
+
+    public Node[] neighbors;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
 	}
 }
